bb19697828b18ba29cfa0826dab1845974f0ea8a
Uzumaki Naruto <96066267+Uzumaki-Naruto-10@users.noreply.github.com>
2022-06-27 19:38:54 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPXrAcqUile06_ZuHzR9OvYt_nii5ln7A",
+    authDomain: "project-100-ab076.firebaseapp.com",
+    databaseURL: "https://project-100-ab076-default-rtdb.firebaseio.com",
+    projectId: "project-100-ab076",
+    storageBucket: "project-100-ab076.appspot.com",
+    messagingSenderId: "495456266252",
+    appId: "1:495456266252:web:31f568337d94aed6eee629"
+  };
+  
+
+  
+firebase.initializeApp(firebaseConfig);
+
